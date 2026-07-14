6bcd3a34178cb4cd529114ea7a3cd8c15b9689c4
Arafat_misba <107274178+arafatmisbawebdeveloper@users.noreply.github.com>
2026-07-14 12:37:33 +0600

    FarmPiggieSolution

---
+++ farmPiggieSubsetSum.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h> //
+using namespace std;
+
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define endl '\n'
+#define OR ||
+typedef double dl;
+typedef long long ll;
+
+void solve()
+{
+     int n;
+     cin>>n;
+     cout<<n << " ";
+     for( int i = 1; i < n; i++ ) {
+        cout<<i<<" ";
+     }
+     cout<<endl;
+}
+int main()
+{
+    optimize();
+    int tc;
+    cin>>tc;
+    while(tc--) {
+        solve();
+    }
+
+}
